7b8fe3e0b4d336f0fcc8ca209a306f5dcf6f8978
minikupa <znvk3768@gmail.com>
2022-10-09 14:42:16 +0900

    액자 크기, 위치 조절

--- digital/flutter_service_worker.js
+++ digital/flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "58daecc6af076b1b962ceb760b176289",
-"/": "58daecc6af076b1b962ceb760b176289",
-"main.dart.js": "e9a4133d43963ce729485108d6093e85",
+"index.html": "5625dfff6214cc67c86a8bf82eb63a69",
+"/": "5625dfff6214cc67c86a8bf82eb63a69",
+"main.dart.js": "8d823e24655b0c6bb18ae2b117e3558d",
 "manifest.json": "d5a49b67b47182efda674c8227af3727",
 "version.json": "b310672db26a67f5b7623f4456963ebd"
 };
