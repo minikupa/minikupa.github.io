c675c4624df35cc9e4118cc3dc6e5f75d02933df
minikupa <minikupa@kakao.com>
2023-03-13 08:32:40 +0900

    [digital] 오늘 중 이미 지난 급식은 표시 X

--- digital/flutter_service_worker.js
+++ digital/flutter_service_worker.js
@@ -76,9 +76,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7faf922c818128d790532114c0692a2d",
-"/": "7faf922c818128d790532114c0692a2d",
-"main.dart.js": "6c2447ea2c913fa8f50bb641e93cff9f",
+"index.html": "00414470ab809f4df29c95af1561916a",
+"/": "00414470ab809f4df29c95af1561916a",
+"main.dart.js": "82f192bfaffcd6bad007794cb8e60709",
 "manifest.json": "d5a49b67b47182efda674c8227af3727",
 "version.json": "b310672db26a67f5b7623f4456963ebd"
 };
